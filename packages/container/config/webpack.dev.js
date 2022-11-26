b879c6dfffc697330e1058cf5000557bb0af54ea
Colin Clark <col.c.clark@gmail.com>
2022-11-26 15:37:51 +0000

    container deploy with GH actions

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
+
 const packageJSON = require('../package.json')
 const commonConfig = require('./webpack.common')
 
@@ -13,10 +13,6 @@ const devConfig = {
     }
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    }),
-
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
