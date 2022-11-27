3411e57598cb63cc667fc45cf20c18047d5cdaf5
Colin Clark <col.c.clark@gmail.com>
2022-11-27 12:47:43 +0000

    add navigation and method of comms between container and marketing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,29 @@
 import React, { useRef, useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import { createBrowserHistory } from 'history'
 
-import { mount as marketingAppMount } from "marketing/MarketingApp"
+import { mount } from "marketing/MarketingApp"
 
 export default () => {
   const ref = useRef(null)
+  const history = useHistory()
 
   useEffect(() => {
-    marketingAppMount(ref.current)
-  })
+    const { onParentNavigate } = mount(ref.current, {
+      defaultHistory: createBrowserHistory(),
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location
+        // communication up from microfrontend
+        if (pathname !== nextPathname) {
+          history.push(nextPathname)
+        }
+      }
+    })
+
+    // communication down to microfrontend
+    history.listen(onParentNavigate)
+  }, [])
+
 
   return <div ref={ref} />
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Switch, Route, BrowserRouter } from "react-router-dom"
+import { Switch, Route, Router } from "react-router-dom"
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles"
 
 import Landing from "./components/Landing"
@@ -10,15 +10,17 @@ const generateClassName = createGenerateClassName({
   productionPrefix: "ma"
 })
 
-export default () => {
-  return <div>
-    <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/pricing" component={Pricing} />
-          <Route path="/" component={Landing} />
-        </Switch>
-      </BrowserRouter>
-    </StylesProvider>
-  </div>
+export default ({ history }) => {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  )
 }
