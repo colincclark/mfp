163dd5992700376a84ee3049a829d5ca776a00de
Colin Clark <col.c.clark@gmail.com>
2022-11-27 13:53:41 +0000

    add auth app

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,40 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import { createBrowserHistory, createMemoryHistory } from 'history'
+
+import App from "./App"
+
+const mount = (el, { defaultHistory, onNavigate }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    // communicate up to the container
+    history.listen(onNavigate)
+  }
+
+  ReactDOM.render(
+    <App history={history} />,
+    el
+  )
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location
+      // communication down from container
+      if (pathname !== nextPathname) {
+        history.push(nextPathname)
+      }
+    }
+  }
+}
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector(".dev-auth-route")
+
+  if (el) {
+    mount(el, { defaultHistory: createBrowserHistory() })
+  }
+}
+
+export { mount }
---
+++ packages/container/src/components/AuthApp.js
@@ -0,0 +1,30 @@
+import React, { useRef, useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import { createBrowserHistory } from 'history'
+
+import { mount } from "auth/AuthApp"
+
+export default () => {
+  const ref = useRef(null)
+  const history = useHistory()
+
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      defaultHistory: createBrowserHistory(),
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location
+        // communication up from microfrontend
+        if (pathname !== nextPathname) {
+          history.push(nextPathname)
+        }
+      }
+    })
+
+    // communication down to microfrontend
+    history.listen(onParentNavigate)
+  }, [])
+
+
+  return <div ref={ref} />
+}
+
